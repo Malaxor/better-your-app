4834dc66cafb1530fc14ecbd9156a71b83a242a1
Malaxor <nogobaduk@gmail.com>
2018-12-13 19:01:19 -0700

    heroku push

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 
     res.send({ hi: 'there'});
 });
 
-app.listen(PORT, () => {
-    console.log("App listening in on PORT 5000");
-});
+app.listen(PORT);
