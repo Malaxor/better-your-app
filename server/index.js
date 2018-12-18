99e2c151cdcd37aa21426a1474e587f1cc5ca6a3
Malaxor <nogobaduk@gmail.com>
2018-12-17 21:03:18 -0700

    working with passport and google authenticate

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
 
-    res.send({ bye: 'buddy'});
-});
+    })
+);
+app.get('/auth/google', passport.authenticate('google', {
+    
+    scope: ['profile', 'email']
+}));
+app.get('/auth/google/callback', passport.authenticate('google', {
+
+
+}));
 
 app.listen(PORT);
