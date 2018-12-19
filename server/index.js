b571699fb4d622feb783f72c8d0af800bbcde8ae
Malaxor <nogobaduk@gmail.com>
2018-12-18 19:30:29 -0700

    refactored code: created two folders (services, routes) and placed code from index.js to files in those two folders; also installed and connected to mongoose

--- server/index.js
+++ server/index.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-
-    })
-);
-app.get('/auth/google', passport.authenticate('google', {
-    
-    scope: ['profile', 'email']
-}));
-app.get('/auth/google/callback', passport.authenticate('google', {
+require('../services/passport');
+require('../routes/authRoutes')(app);
 
 
-}));
 
 app.listen(PORT);
