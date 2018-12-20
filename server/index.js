fc58bb9ec9c35a81b4a69764e9d5a6f251938d90
Malaxor <nogobaduk@gmail.com>
2018-12-19 17:06:04 -0700

    created mongoose User model; User model will be saved whenever a User logs in

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
+require('../models/User');
 require('../services/passport');
 require('../routes/authRoutes')(app);
 
-
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -10,5 +12,7 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
 
+        new User ({ googleId: profile.id }).save();
+        // console.log(profile);
     })
 );
