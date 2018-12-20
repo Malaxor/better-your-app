8197d1bf4cc8d2dcec44441ea43acae956a1444a
Malaxor <nogobaduk@gmail.com>
2018-12-19 19:04:09 -0700

    passport/google authentication complete: serialze and deserialize users; view current user and logout

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 
 const express = require('express');
 const app = express();
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 require('../models/User');
 require('../services/passport');
+app.use(cookieSession({
+    
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days (in milliseconds)
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 require('../routes/authRoutes')(app);
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
